827746148a96651f751ce19b86f1d71e4191bbd2
DhouhaNBIBA <dhouha.nbiba@gmail.com>
2022-11-02 16:03:35 +0100

    fixed-erreur(naming)

--- Python-Flask/Week1/Day4/core-assignment/Playground/server.py
+++ Python-Flask/Week1/Day4/core-assignment/Playground/server.py
@@ -6,11 +6,11 @@ def main():
 
 @app.route('/play/<int:x>')
 def play(x):
-    return render_template('play.html', plays=plays)
+    return render_template('play.html', plays=x)
 
 @app.route('/play/<int:x>/<string:color>')
 def changer_color(x,color):
-    return render_template('color.html',plays=plays,couleur=coleur)
+    return render_template('color.html',plays=x,couleur=color)
 
 if __name__==('__main__'):
     app.run(debug=True)
