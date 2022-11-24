922514821574a94dc44e09315e883cbfb7c622c6
DhouhaNBIBA <dhouha.nbiba@gmail.com>
2022-11-24 22:40:45 +0100

    first-commit

--- MERN/Week1/Day4/practice-assignment/prop_it_up/src/App.js
+++ MERN/Week1/Day4/practice-assignment/prop_it_up/src/App.js
@@ -10,7 +10,7 @@ function App() {
   }
   const person2= {
     first_name: "Smith",
-    last_name: "JaneJohn",
+    last_name: "John",
     age: 88,
     hairColor: "Brown",
 }
@@ -28,13 +28,14 @@ const person4= {
 }
   return (
   <div className="App">
-    <h1>More State</h1>
+    {/* <h1>More State</h1> */}
     {/* //* every componenet has its own state */}
     {/* <ClassComponent num={10} person={person} /> */}
-    <PersonCard person1={person1} />
+    {/* <PersonCard person1={person1} />
     <PersonCard person2={person2} />
     <PersonCard person3={person3} />
-    <PersonCard person4={person4} />
+    <PersonCard person4={person4} /> */}
+    <PersonCard person1={person1}  person2={person2} person3={person3} person4={person4}/>
   </div>
 );
 }
--- MERN/Week1/Day4/practice-assignment/prop_it_up/src/components/PersonCard.jsx
+++ MERN/Week1/Day4/practice-assignment/prop_it_up/src/components/PersonCard.jsx
@@ -13,12 +13,36 @@ export default class PersonCard extends Component{
     }
     render() {
         return (
-            <fieldset>
-                <legend>PersonCard.jsx</legend>
+            <div> 
+                {}
                 <div>
-                    {/* {this.state.person.name} */}
+                    {/* <legend type='hidden'>Person.jsx</legend> */}
+                    <center>
+                        <h3> {this.props.person1.first_name} , {this.props.person1.last_name}</h3>
+                        <p> {this.props.person1.age}</p>
+                        <p>  {this.props.person1.hairColor} </p>
+                    </center>
                 </div>
-            </fieldset>
+                <div>
+                    {/* <legend>Person.jsx</legend> */}
+                        <h3> {this.props.person2.first_name} , {this.props.person2.last_name}</h3>
+                        <p> {this.props.person2.age}</p>
+                        <p>{this.props.person2.hairColor} </p>
+                    
+                </div>
+                <div>
+                    {/* <legend>Person.jsx</legend> */}
+                        <h3>{this.props.person3.first_name} , {this.props.person3.last_name}</h3>
+                        <p>{this.props.person3.age}</p>
+                        <p>{this.props.person3.hairColor} </p>
+                </div>
+                <div>
+                    {/* <legend>Person.jsx</legend> */}
+                        <h3>{this.props.person4.first_name} , {this.props.person4.last_name}</h3>
+                        <p> {this.props.person4.age}</p>
+                        <p> {this.props.person4.hairColor} </p>
+                </div>
+            </div>
         )
     }
 }
