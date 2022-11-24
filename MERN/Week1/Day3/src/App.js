17590d1cbc99cbc2c1d8a9516bd5be6987db06d2
DhouhaNBIBA <70754078+DhouhaNBIBA@users.noreply.github.com>
2022-11-24 13:32:55 +0100

    Add files via upload

---
+++ MERN/Week1/Day3/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FirstJsx from './Components/first_jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstJsx />
+    </div>
+  );
+}
+
+export default App;
