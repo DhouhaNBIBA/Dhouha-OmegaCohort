1f10d730ecace132e8bc67fd082917af1a2f58f3
DhouhaNBIBA <dhouha.nbiba@gmail.com>
2022-12-04 19:24:53 +0100

    first-commit

--- MERN/Week2/Day1/core-assignment/to-do-liste/src/components/TodoList.jsx
+++ MERN/Week2/Day1/core-assignment/to-do-liste/src/components/TodoList.jsx
@@ -1,66 +1,89 @@
-import React, {useState, useEffect} from 'react';
-import uuid from'react-uuid';
+import React, { useState, useEffect } from 'react';
+import './TodoList.css';
+import uuid from 'react-uuid';
 
-function TodoList() {
-    const[content, setContent]=useState('');
-    const[list, setList]=useState(JSON.parse(localStorage.getItem('tasks')) || []);
-    //GET INPUT VALUE
-    const onChangeHandler = (e) =>{
-        setContent(e.target.value);
-        console.log(setContent);
-
-    }
-    //CREATE NEW TASK
-    const onSubmitHandler = e =>{
-        e.preventDefault();
-        if (content){
-        setList([...list, {content, id: uuid(), isCompleted: false}]);
-    //EMpty Input
-        setContent('');}
-}
-    const removeTask= id => {
-        const filterList = list.filter(task => task.id !== id);
-        setList(filterList);
-    }
-    //ADD TASK TO LOCALSTORAGe
-    useEffect(()=>{
-        localStorage.setItem('tasks', JSON.stringify(list));
-    }, [list])
-    return (
-        <div>
-            <form  onSubmit={onSubmitHandler}>
-                <div>
-                    <input type="text" name='content' value={content}  onChange={ onChangeHandler }/>
-                    <input type="submit" value='Add'/>
-                </div>
-            </form>
-        
-        <div>
-            {list.map((task)=>{
-            return(
-                <div className='task' key={task.id}>
-                    <p style={task.isCompleted  ?{textDecoration: 'line-through'}
-                    : {textDecoration:'none'}
-                    }>{task.content}</p>
-                    <input type="checkbox"
-                    checked={task.isCompleted}
-                    onChange={e => {
-                        setList(prevList =>{
-                            const newList= prevList.map(item =>
-                                item.content === task.content ?
-                                {...item, isCompleted: ! item.isCompleted} : item
-                            );
-                                return newList;
-                            
-                        });
-                    }} />
-                    <button onClick={()=>removeTask(task.id)}>Delete</button>
-                </div>
-                );
-            })}
-        </div>
-        </div>
+const TodoList = () => {
+    const [content, setContent] = useState('');
+    const [list, setList] = useState(
+        JSON.parse(localStorage.getItem('tasks')) || []
     );
-}
 
-export default TodoList
+  // GET INPUT VALUE
+const onChangeHandler = (e) => {
+    setContent(e.target.value);
+    };
+
+  // CREATE NEW TASK
+const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (content) {
+        setList([
+        ...list,
+        { content, id: uuid(), isCompleted: false }, // new Date().getTime().toString()
+    ]);
+
+      // EMPTY INPUT
+    setContent('');
+    }
+    };
+
+  // DELETE TASK
+const removeTask = (id) => {
+    const filtredList = list.filter((task) => task.id !== id);
+    setList(filtredList);
+    };
+
+  // ADD TASK TO LOCALSTORAGE
+useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(list));
+    }, [list]);
+
+    return (
+    <div className="main">
+        <div className="form">
+            <form onSubmit={onSubmitHandler}>
+            <input
+                type="text"
+                name="content"
+                value={content}
+                onChange={onChangeHandler}
+            />
+            <input type="submit" value="Add" />
+            </form>
+        </div>
+        <div className="tasks">
+            {list.map((task) => {
+            return (
+                <div className="task" key={task.id}>
+                <p
+                    style={
+                    task.isCompleted
+                        ? { textDecoration: 'line-through' }
+                        : { textDecoration: 'none' }
+                    }
+                >
+                    {task.content}
+                </p>
+                <input
+                    type="checkbox"
+                    checked={task.isCompleted}
+                    onChange={(e) => {
+                    setList((prevList) => {
+                        const newList = prevList.map((item) =>
+                        item.content === task.content
+                            ? { ...item, isCompleted: !item.isCompleted }
+                            : item
+                        );
+                        return newList;
+                    });
+                    }}
+                />
+                <button onClick={() => removeTask(task.id)}>Delete</button>
+                </div>
+            );
+        })}
+        </div>
+    </div>
+    );
+};
+export default TodoList;
