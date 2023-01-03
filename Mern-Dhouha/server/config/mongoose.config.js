a35be79193065e95fb67ae96b9e7664d802936e5
DhouhaNBIBA <dhouha.nbiba@gmail.com>
2023-01-03 11:48:12 +0100

    first-commit

--- Mern-Dhouha/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-module.exports = (DB_NAME) => {
-mongoose.connect(`mongodb://localhost/${DB_NAME}`)
-
-    .then(() => console.log("Established a connection to the database"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err))
-}
--- Mern-Dhouha/server/routes/pirates.routes.js
+++
@@ -1,9 +0,0 @@
-const PirateController = require("../controllers/pirates.controllers");
-
-module.exports = app => {
-        app.get("/api/pirates", PirateController.findAllPirates);
-        app.get("/api/pirates/:id", PirateController.findOnePirate);
-//     app.put("/api/pirates/:id/reviews", PirateController.addReview);
-        app.post("/api/pirates/new", PirateController.createPirate);
-        app.delete("/api/pirates/delete/:id", PirateController.deletePirate);
-};
